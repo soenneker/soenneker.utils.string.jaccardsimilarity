2f7032cde37ba3edc3c21cc6afc628b10d5d2a0e
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-22 19:51:05 -0500

    Various fixes

--- test/Soenneker.Utils.String.JaccardSimilarity.Tests/JaccardSimilarityStringUtilTests.cs
+++ test/Soenneker.Utils.String.JaccardSimilarity.Tests/JaccardSimilarityStringUtilTests.cs
@@ -11,18 +11,18 @@ public class JaccardSimilarityStringUtilTests : HostedUnitTest
     {
     }
 
-    [Theory]
-    [InlineData("", "", 1)]
-    [InlineData("abc", "", 0)]
-    [InlineData("", "xyz", 0)]
-    [InlineData("kitten", "sitting", 0)]
-    [InlineData("kitten", "kitten", 1)]
-    [InlineData("abc", "def", 0)]
-    [InlineData("abcdef", "abc", 0)]
-    [InlineData("abc", "abcd", 0)]
-    [InlineData("this is sitting on a porch", "this is sitting", .50)]
-    [InlineData("the cat sat on a hat", "sad on a hat", .4285)]
-    [InlineData("this is a test", "this is another test", .6)]
+    [Test]
+    [Arguments("", "", 1)]
+    [Arguments("abc", "", 0)]
+    [Arguments("", "xyz", 0)]
+    [Arguments("kitten", "sitting", 0)]
+    [Arguments("kitten", "kitten", 1)]
+    [Arguments("abc", "def", 0)]
+    [Arguments("abcdef", "abc", 0)]
+    [Arguments("abc", "abcd", 0)]
+    [Arguments("this is sitting on a porch", "this is sitting", .50)]
+    [Arguments("the cat sat on a hat", "sad on a hat", .4285)]
+    [Arguments("this is a test", "this is another test", .6)]
     public void CalculateSimilarity_Returns_Correct_Similarity_Score(string str1, string str2, double expectedScore)
     {
         double similarityScore = JaccardSimilarityStringUtil.CalculateSimilarity(str1, str2);
@@ -30,15 +30,15 @@ public class JaccardSimilarityStringUtilTests : HostedUnitTest
         similarityScore.Should().BeApproximately(expectedScore, 0.001);
     }
 
-    [Theory]
-    [InlineData("", "", 100.0)]
-    [InlineData("abc", "", 0.0)]
-    [InlineData("", "xyz", 0.0)]
-    [InlineData("kitten", "sitting", 0)]
-    [InlineData("kitten", "kitten", 100.0)]
-    [InlineData("abc", "def", 0.0)]
-    [InlineData("abcdef", "abc", 0.0)]
-    [InlineData("this is sitting on a porch", "this is sitting", 50)]
+    [Test]
+    [Arguments("", "", 100.0)]
+    [Arguments("abc", "", 0.0)]
+    [Arguments("", "xyz", 0.0)]
+    [Arguments("kitten", "sitting", 0)]
+    [Arguments("kitten", "kitten", 100.0)]
+    [Arguments("abc", "def", 0.0)]
+    [Arguments("abcdef", "abc", 0.0)]
+    [Arguments("this is sitting on a porch", "this is sitting", 50)]
     public void CalculateSimilarityPercentage_Returns_Correct_Percentage(string str1, string str2, double expectedPercentage)
     {
         double similarityPercentage = JaccardSimilarityStringUtil.CalculateSimilarityPercentage(str1, str2);
